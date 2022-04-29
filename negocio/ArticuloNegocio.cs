7e59d1f7b12f68c87c57a2172148fb1172173673
lvingman <45737515+lvingman@users.noreply.github.com>
2022-04-28 21:04:52 -0300

    Create ArticuloNegocio.cs
    Agrega clase ArticuloNegocio

---
+++ negocio/ArticuloNegocio.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using dominio;
+
+namespace articulo
+{
+    public class ArticuloNegocio
+    {
+        public List<Articulo> listar()
+        {
+            List<Articulo> lista = new List<Articulo>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("select id, descripcion as nombreArticulo from ARTICULOS");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Marca aux = new Marca();
+                    aux.ID = (int)datos.Lector["id"];
+                    aux.Descripcion = (string)datos.Lector["nombreArticulo"];
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+
+    }
+}
